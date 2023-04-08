7703d90464ccbeb647ddeb636470339962df0810
AAQIB ANAYAT <computermac58@gmail.com>
2023-04-08 14:43:16 +0530

    added fontowsome link and google fonts link

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- links to external css files -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;700&display=swap" rel="stylesheet">
+   
     <link rel="stylesheet" href="style.css"  />
 
     <title>E Shop</title>
@@ -13,6 +16,7 @@
 <body>
     <h3>HOME PAGE</h3>
     <section id="main">
+    <i class="fa fa-regular fa-square"></i>
         body sections
     </section>
 
